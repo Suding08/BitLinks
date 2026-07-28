68493114cd4effe9932e32df036dfd1345707752
Suding08 <gsudin52@gmail.com>
2026-07-28 23:07:14 +0530

    Update Footer.js

--- component/Footer.js
+++ component/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 function Footer() {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (
-    
-    <footer className='bg-purple-600 text-black flex justify-center items-center px-4 h-15'>
+    <footer className='bg-[#254f1a] text-white flex justify-center items-center px-4 h-15 bottom-0 left-0 w-full sticky'>
         <p className='text-center'>Copyright &copy; {currentYear} BitLinks - All rights reserved</p>
     </footer>
   )
